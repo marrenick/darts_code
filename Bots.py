8ca7c3331f8a5ed5a963a38c5b826b62633a68a2
marrenick <145130036+marrenick@users.noreply.github.com>
2024-05-10 10:16:17 +0200

    foutjes uit merge opgelost

--- Bots.py
+++ Bots.py
@@ -18,24 +18,26 @@ class Bots:
         self.statisticsman = statisticsMan(data)
 
     def throw(self, player, aimed_at_number, aimed_at_section):
-        x_centre, y_centre, section = self.dartboard.get_coordinates_from_throw(aimed_at_number, aimed_at_section)
-        print(x_centre,y_centre)
+        x_centre, y_centre = self.dartbord.get_coordinates_from_throw(aimed_at_number, aimed_at_section)
         cov = self.statisticsman.calculateCovarianceMatrix(player, aimed_at_number, aimed_at_section).mul(
             self.difficulty)
         print(cov)
         x, y = np.random.multivariate_normal([x_centre, y_centre], cov, 1).T
         throws = [self.dartboard.calculate_dart_score(x_throw, y_throw) for x_throw in x for y_throw in y]
-        return throws[0], section
+        sections = [self.dartbord.calculate_dart_section(x_throw, y_throw) for x_throw in x for y_throw in y]
+        return throws[0],sections[0]
 
 
 if __name__ == '__main__':
     player = input('Choose opponent:')
     difficulty = 0.05
+
     bot = Bots(player, difficulty)
     while True:
         input('Press enter to throw again.')
         throw, section = bot.throw(player, 16, 'DOUBLE')
         print(str(throw))
+        print(section)
 
 
     def temp(self):
