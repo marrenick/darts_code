2193d13347b2ce4e94bcddf043ae788206f13f66
WarreGevers <s0355721@gmail.com>
2024-05-10 09:00:15 +0200

    bot geeft nu ook de section bij throw, nog niet getest omdat db plat ligt

--- Bots.py
+++ Bots.py
@@ -17,25 +17,27 @@ class Bots:
         self.statisticsman = statisticsMan(data)
 
     def throw(self, player, aimed_at_number, aimed_at_section):
-        x_centre, y_centre, section = self.dartbord.get_coordinates_from_throw(aimed_at_number, aimed_at_section)
-        print(x_centre,y_centre)
+        x_centre, y_centre = self.dartbord.get_coordinates_from_throw(aimed_at_number, aimed_at_section)
+
         cov = self.statisticsman.calculateCovarianceMatrix(player, aimed_at_number, aimed_at_section).mul(
             self.difficulty)
         print(cov)
         x, y = np.random.multivariate_normal([x_centre, y_centre], cov, 1).T
         throws = [self.dartbord.calculate_dart_score(x_throw, y_throw) for x_throw in x for y_throw in y]
-        return throws[0], section
+        sections = [self.dartbord.calculate_dart_section(x_throw, y_throw) for x_throw in x for y_throw in y]
+        return throws[0],sections[0]
 
 
 if __name__ == '__main__':
     player = input('Choose opponent:')
     difficulty = 0.05
+
     bot = Bots(player, difficulty)
     while True:
         input('Press enter to throw again.')
         throw, section = bot.throw(player, 16, 'DOUBLE')
         print(str(throw))
-
+        print(section)
 
     def temp(self):
         plt.figure(1)
